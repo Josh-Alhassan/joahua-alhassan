5725c2021885e6f8bfffc650ab1a4e7fa5f170b1
josh-alhassan <saintjosh26@gmail.com>
2024-12-31 13:28:34 +0100

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <Home />
+    </div>
+  );
+}
+
+export default App;

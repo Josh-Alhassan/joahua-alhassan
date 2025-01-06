917bff93fee4f6c0a57c80e75ab96a03cba7e7c2
josh-alhassan <saintjosh26@gmail.com>
2025-01-06 22:54:47 +0100

    add blog top section

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import PrimaryHeader from "../utilities/PrimaryHeader";
+
+import style from "./Blog.module.css";
 
 function Blog() {
   return (
     <div>
       <Navigation />
-      BLOG
+      <PrimaryHeader fontWeight="500">My Blogs</PrimaryHeader>
+      <a href="#" className={style.blogLink}>
+        DATE
+        <span>Blog Title</span>
+      </a>
     </div>
   );
 }

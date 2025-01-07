cce4711cc2d7deb3cec0d6c72d9b57e206d2a41e
Josh-Alhassan <saintjosh26@gmail.com>
2025-01-07 02:30:25 +0100

    add list of articles data

--- src/data/data.js
+++ src/data/data.js
@@ -34,3 +34,106 @@ export const menuItems = [
   { item: "Blog", link: "/blog" },
   { item: "Contact", link: "/contact" },
 ];
+
+export const blogPosts = [
+  {
+    title: "Understanding the useEffect Hook in React: A Comprehensive Guide",
+    duration: "December 11, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/understanding-the-useeffect-hook-in-react-a-comprehensive-guide",
+  },
+  {
+    title: "State Update Batching in React made simple",
+    duration: "December 11, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/state-update-batching-in-react-made-simple",
+  },
+  {
+    title:
+      "Understanding Render Logic, Event Handlers, and Side Effects in React",
+    duration: "December 10, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/understanding-render-logic-event-handlers-and-side-effects-in-react",
+  },
+  {
+    title: "Components vs Instances vs Elements",
+    duration: "November 13, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/components-vs-instances-vs-elements",
+  },
+  {
+    title: "Understanding JavaScript Implementation: A Note-Based Article",
+    duration: "November 12, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/understanding-javascript-implementation-a-note-based-article",
+  },
+  {
+    title:
+      "My Note on Understanding PropTypes in React: A Guide to Component Typing",
+    duration: "November 2, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/my-note-on-understanding-proptypes-in-react-a-guide-to-component-typing",
+  },
+  {
+    title:
+      "From English Teacher to Tech Expert: How Anjana Vakil’s Non-Linear Journey Shaped Her Career in Developer Education",
+    duration: "November 1, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/from-english-teacher-to-tech-expert-how-anjana-vakils-non-linear-journey-shaped-her-career-in-developer-education",
+  },
+  {
+    title: "Understanding Component Composition in React",
+    duration: "Octorber 31, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/understanding-component-composition-in-react",
+  },
+  {
+    title:
+      "Understanding React Component Categories: Stateless, Stateful, and Structural Components",
+    duration: "Octorber 29, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/understanding-react-component-categories-stateless-stateful-and-structural-components",
+  },
+  {
+    title: "Optimizing Performance Through Sleep and Brain Health",
+    duration: "Octorber 19, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/optimizing-performance-through-sleep-and-brain-health",
+  },
+  {
+    title: "Developer Health OS and Burnout Recovery Strategies",
+    duration: "Octorber 17, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/developer-health-os-and-burnout-recovery-strategies",
+  },
+  {
+    title: "G2i's Business Model and Commitment to Developer Health",
+    duration: "Octorber 17, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/g2is-business-model-and-commitment-to-developer-health",
+  },
+  {
+    title: "Stress Management and Developer Health",
+    duration: "Octorber 17, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/stress-management-and-developer-health",
+  },
+  {
+    title: "Concept of Developer Health",
+    duration: "Octorber 10, 2024",
+    url: "https://alhassanjoshua.hashnode.dev/concept-of-developer-health",
+  },
+  {
+    title:
+      "Deep Dive into Chrome Alarm API: Scheduling Timed Events in Chrome Extension",
+    duration: "May 30, 2023",
+    url: "https://alhassanjoshua.hashnode.dev/deep-dive-into-chrome-alarm-api-scheduling-timed-events-in-chrome-extensions",
+  },
+  {
+    title: "A Guide to Chrome Extension Configuration: Manifest.json File",
+    duration: "May 30, 2023",
+    url: "https://alhassanjoshua.hashnode.dev/a-guide-to-chrome-extension-configuration-manifestjson-file",
+  },
+  {
+    title: "Crucial Guides to Grow Community Enagement",
+    duration: "December 29, 2022",
+    url: "https://alhassanjoshua.hashnode.dev/deep-dive-into-chrome-alarm-api-scheduling-timed-events-in-chrome-extensions",
+  },
+  {
+    title: "Building Community Engagement",
+    duration: "December 27, 2022",
+    url: "https://alhassanjoshua.hashnode.dev/building-community-engagementhttps://alhassanjoshua.hashnode.dev/deep-dive-into-chrome-alarm-api-scheduling-timed-events-in-chrome-extensions",
+  },
+  {
+    title: "The Best Metrics for Developer Relation Teams",
+    duration: "November 29, 2022",
+    url: "https://alhassanjoshua.hashnode.dev/the-best-metrics-for-developer-relation-teams",
+  },
+];
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,18 +1,30 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import PrimaryHeader from "../utilities/PrimaryHeader";
-
 import style from "./Blog.module.css";
+import { blogPosts } from "../data/data";
+import Footer from "../components/Footer";
 
 function Blog() {
   return (
     <div>
       <Navigation />
       <PrimaryHeader fontWeight="500">My Blogs</PrimaryHeader>
-      <a href="#" className={style.blogLink}>
-        DATE
-        <span>Blog Title</span>
-      </a>
+      <div>
+        {blogPosts.map((post, index) => (
+          <a
+            key={index}
+            href={post.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={style.blogLink}
+          >
+            <p>{post.duration}</p>
+            <p style={{ fontWeight: "400" }}>{post.title}</p>
+          </a>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 }

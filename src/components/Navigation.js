6ac7b9153afc85ee25ad362700c69ed8999cf00b
Josh-Alhassan <saintjosh26@gmail.com>
2025-01-07 09:35:45 +0100

    add mobile navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuItem from "../utilities/MenuItem";
 
 import { menuItems } from "../data/data";
+import style from "./Navigation.module.css";
 
 function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
   return (
-    <div
-      style={{
-        marginBottom: "40px",
-        display: "flex",
-        justifyContent: "center",
-        gap: "20px",
-      }}
-    >
-      {menuItems.map((menu, idx) => (
-        <MenuItem key={idx} link={menu.link}>
-          {menu.item}
-        </MenuItem>
-      ))}
-    </div>
+    <nav className={style.navContainer}>
+      {/* Hamburger Icon */}
+      <button
+        className={style.hamburger}
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
+        <span className={style.bar}></span>
+        <span className={style.bar}></span>
+        <span className={style.bar}></span>
+      </button>
+      {/* Menu Items */}
+      <div className={`${style.menu} ${isMenuOpen ? style.open : ""}`}>
+        {menuItems.map((menu, idx) => (
+          <MenuItem key={idx} link={menu.link}>
+            {menu.item}
+          </MenuItem>
+        ))}
+      </div>
+    </nav>
   );
 }
 
